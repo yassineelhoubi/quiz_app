18c218868deb1df61cdba2cfaa287f92f6aebbd2
Yassine ELHOUBI <elhoubiyassine@gmail.com>
2024-02-28 18:25:51 +0100

    adding statefulwidget

---
+++ lib/quiz.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/statrt_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 78, 13, 151),
+              Color.fromARGB(255, 107, 15, 168),
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+          ),
+          child: const StatrtScreen(),
+        ),
+      ),
+    );
+  }
+}
