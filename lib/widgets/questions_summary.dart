42fdd5abb1a59c1ba73a54c70327565f36f816b9
Yassine ELHOUBI <elhoubiyassine@gmail.com>
2024-03-01 18:58:44 +0100

    expended the summary data width

--- lib/widgets/questions_summary.dart
+++ lib/widgets/questions_summary.dart
@@ -11,15 +11,17 @@ class QuestionsSummary extends StatelessWidget {
         return Row(
           children: [
             Text(((data["index"] as int) + 1).toString()),
-            Column(
-              children: [
-                Text(data["question"] as String),
-                const SizedBox(
-                  height: 5,
-                ),
-                Text(data["correct_answer"] as String),
-                Text(data["user_answer"] as String),
-              ],
+            Expanded(
+              child: Column(
+                children: [
+                  Text(data["question"] as String),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text(data["correct_answer"] as String),
+                  Text(data["user_answer"] as String),
+                ],
+              ),
             )
           ],
         );
