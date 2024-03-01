9c0f6537849cd54edca70d0bda2772759e7f0695
Yassine ELHOUBI <elhoubiyassine@gmail.com>
2024-03-01 16:09:57 +0100

    add condition to clear the selectedAnswers array and back to the startScreen

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
 import 'package:quiz_app/questions_screen.dart';
 import 'package:quiz_app/start_screen.dart';
 
@@ -27,6 +28,12 @@ class _QuizState extends State<Quiz> {
 
   void chooseAnswer(String answer) {
     selectedAnswers.add(answer);
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        selectedAnswers.clear();
+        activeScreen = StartScreen(switchScreen);
+      });
+    }
   }
 
   @override
